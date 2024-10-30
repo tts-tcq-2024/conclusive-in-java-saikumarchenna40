c059de9c0f5d5c479aaeaff6a5903344a6eb5156
saikumarchenna <121084246+saikumarchenna40@users.noreply.github.com>
2024-10-30 18:18:22 +0530

    Update CoolingContextTest.java

--- src/test/java/TypewiseAlert/CoolingContextTest.java
+++ src/test/java/TypewiseAlert/CoolingContextTest.java
@@ -1,7 +1,7 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 // Mock implementation of the CoolingStrategy interface for testing
 class MockCoolingStrategy implements CoolingStrategy {
