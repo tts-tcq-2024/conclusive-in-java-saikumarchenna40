4f9453f3d54da894b37af59a4e4a699ef983dc76
saikumarchenna <121084246+saikumarchenna40@users.noreply.github.com>
2024-10-30 18:16:03 +0530

    Update CoolingControllerTest.java

--- src/test/java/TypewiseAlert/CoolingControllerTest.java
+++ src/test/java/TypewiseAlert/CoolingControllerTest.java
@@ -1,5 +1,5 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
 
 //import static org.junit.jupiter.api.Assertions.*;
 
