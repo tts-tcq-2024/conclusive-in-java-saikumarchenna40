f5fc83b3e23e989e15a5433a9349964dc07c8977
saikumarchenna <121084246+saikumarchenna40@users.noreply.github.com>
2024-10-30 11:20:45 +0530

    Update CoolingContextTest.java

--- src/test/java/TypewiseAlert/CoolingContextTest.java
+++ src/test/java/TypewiseAlert/CoolingContextTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
